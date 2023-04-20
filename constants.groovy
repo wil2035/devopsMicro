edef482e4c4a619cccaade996a90077d4641fcea
Wilmer Oidor <wilmeroidor.globant@latam.com>
2023-04-20 16:38:05 -0500

    code clean up2

--- constants.groovy
+++ constants.groovy
@@ -1,15 +1,11 @@
 def ENVIRONMENTS = [
     'dev': [
-        'host': 'localhosr',
-        'user': 'jenkins',
-        'port': 22,
+        'host': 'localhost',
         'docker_registry': 'dev-registry.example.com',
         'tag': 'dev'
     ],
     'prod': [
         'host': 'prod',
-        'user': 'jenkins',
-        'port': 22,
         'docker_registry': 'prod-registry.example.com',
         'tag': 'dev'
     ]
